6af3eae4e7146faaf5f91d8b0bbdbfb39d687f20
Adil Jawed <Aj2010adil@gmai.com>
2025-08-28 22:51:11 +0530

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'vite-plugin-tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vitejs.dev/config/
